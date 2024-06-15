ecd6b1bbf59b2e7ae5172cadba3f8150f925d659
Erlend <erlend.eelmets@gmail.com>
2024-06-15 11:49:53 +0300

    3.1

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.json(data)
+app.get("/api/persons", (request, response) => {
+  response.json(data);
+});
+
+app.get('/info', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
   })
 
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
