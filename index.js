4835e76fcbdb1b603bfcec55bfb3cba8d54d14dd
Erlend <erlend.eelmets@gmail.com>
2024-06-15 14:03:30 +0300

    3.6

--- index.js
+++ index.js
@@ -35,20 +35,38 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.post('/api/persons', (request, response) => {
-    const id = Math.floor(Math.random() * 9000)
-    const person = request.body;
-    person.id = id;
-    data = data.concat(person);
-    response.json(person);
+app.post("/api/persons", (request, response) => {
+  const id = Math.floor(Math.random() * 9000);
+  const person = request.body;
 
-})
+  if (
+    !Object.values(person).includes("name") ||
+    !Object.values(person).includes("number")
+  ) {
+    response
+      .status({ status: 400, statusText: "The name or number is missing" })
+      .end();
+  }
+
+  if (
+    typeof data.find((existing) => existing.name === person.name) !==
+    "undefined"
+  ) {
+    response
+      .status({ status: 400, statusText: "The name must be unique" })
+      .end();
+  }
+
+  person.id = id;
+  data = data.concat(person);
+  response.json(person);
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    data = data.filter((person) => person.id !== id)
-    response.status(204).end()
-})
+  const id = Number(request.params.id);
+  data = data.filter((person) => person.id !== id);
+  response.status(204).end();
+});
 
 app.get("/api/persons", (request, response) => {
   response.json(data);
