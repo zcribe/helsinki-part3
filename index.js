fec510f480710bb2db546d94af031cf0a5aae6b5
Erlend <erlend.eelmets@gmail.com>
2024-06-15 15:14:21 +0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
+
+
 const app = express();
 
 
+app.use(cors())
 app.use(express.json());
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
