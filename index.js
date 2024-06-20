23a65986b42bf768a135b7c1d3a36ee92ecba442
Erlend <erlend.eelmets@gmail.com>
2024-06-20 15:36:24 +0300

    3.13, 3.14, 3.15, 3.16

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
+const URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
+app.use(cors());
+
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
@@ -15,75 +22,47 @@ app.use(
 );
 
 
-
-let data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = data.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+// Persons
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((result) => {
+    response.json(result);
+  });
 });
 
-app.use(express.static("dist"));
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then(result => {
+    if (result){
+      response.json(result);
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
-  const id = Math.floor(Math.random() * 9000);
-  const person = request.body;
+  const body = request.body;
 
-  if (!"name" in person || !"number" in person) {
-    response
-      .status({ status: 400, statusText: "The name or number is missing" })
-      .end();
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  if (
-    typeof data.find((existing) => existing.name === person.name) !==
-    "undefined"
-  ) {
-    response
-      .status({ status: 400, statusText: "The name must be unique" })
-      .end();
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number || "",
+  });
 
-  person.id = id;
-  data = data.concat(person);
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  data = data.filter((person) => person.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id).then((result) => {
+    response.json(result);
+  });
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(data);
-});
+// Info
 
 app.get("/info", (request, response) => {
   response.send(
@@ -91,7 +70,21 @@ app.get("/info", (request, response) => {
   );
 });
 
-const PORT = 3001;
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const Person = require('./models/person')
+
 if (process.argv.length < 3) {
   console.log("give password as argument");
   process.exit(1);
@@ -11,13 +13,6 @@ const DBURL = `mongodb+srv://erlendeelmets:${password}@helsinki.nlyioyo.mongodb.
 
 mongoose.set("strictQuery", false);
 mongoose.connect(DBURL);
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-const Person = mongoose.model("Person", personSchema);
 
 const name = process.argv[3];
 const number = process.argv[4];
