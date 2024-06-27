662ef38f6e9fc49dfe488532e737398427669cff
Erlend <erlend.eelmets@gmail.com>
2024-06-27 16:20:39 +0300

    3.20, 3.21

--- models/person.js
+++ models/person.js
@@ -16,7 +16,15 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{7,8}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
